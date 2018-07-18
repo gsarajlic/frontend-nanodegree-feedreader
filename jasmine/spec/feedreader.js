f5d3be5a8711d96f6a8b55ab3291c4578420bf0e
Goran <gsarajlic@gmail.com>
2018-07-18 09:38:14 +0200

    Changes made as per requirement of Udacity review

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -27,8 +27,8 @@ $(function() {
         });
 
 
-        /* TODO: Write a test that loops through each feed
-         * in the allFeeds object and ensures it has a URL defined
+        /* 
+         * Test ensures it has a URL defined
          * and that the URL is not empty.
          */
         
@@ -39,8 +39,8 @@ $(function() {
             });
         });
 
-        /* TODO: Write a test that loops through each feed
-         * in the allFeeds object and ensures it has a name defined
+        /* 
+         * Test ensures it has a name defined
          * and that the name is not empty.
          */
         it('FEED NAME exists', function (){
@@ -52,11 +52,11 @@ $(function() {
     });
 
 
-    /* TODO: Write a new test suite named "The menu" */
+    
 
     describe('The menu', function (){
     
-        /* TODO: Write a test that ensures the menu element is
+        /* Test ensures the menu element is
          * hidden by default. You'll have to analyze the HTML and
          * the CSS to determine how we're performing the
          * hiding/showing of the menu element.
@@ -66,7 +66,7 @@ $(function() {
          });
 
 
-         /* TODO: Write a test that ensures the menu changes
+         /* The menu changes
           * visibility when the menu icon is clicked. This test
           * should have two expectations: does the menu display when
           * clicked and does it hide when clicked again.
@@ -88,7 +88,7 @@ $(function() {
     /* TODO: Write a new test suite named "Initial Entries" */
     describe('Initial Entries', function(){
     
-        /* TODO: Write a test that ensures when the loadFeed
+        /* When the loadFeed
          * function is called and completes its work, there is at least
          * a single .entry element within the .feed container.
          * Remember, loadFeed() is asynchronous so this test will require
@@ -105,15 +105,16 @@ $(function() {
          * .feed container.
          */
         it('contain at least one entry within FEED', function() {
-            expect($('.entry').length).toBeGreaterThan(0);
+            expect($('.feed .entry').length).toBeGreaterThan(0);
         });
 
     });
     
-    /* TODO: Write a new test suite named "New Feed Selection" */
-     /* TODO: Write a test that ensures when a new feed is loaded
-         * by the loadFeed function that the content actually changes.
-         * Remember, loadFeed() is asynchronous.
+
+        /* 
+         * When a new feed is loaded
+         * by the loadFeed function that content actually changes.
+         * 
          */
     
         describe('New Feed Selection', function() {
